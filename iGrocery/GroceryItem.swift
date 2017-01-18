9c6141006c70a55de029a76044896696b8125e92
ANI <ani@gm009-4.local>
2017-01-17 19:32:22 -0500

    Added GroceryItem class.

---
+++ iGrocery/GroceryItem.swift
@@ -0,0 +1,27 @@
+//
+//  GroceryItem.swift
+//  iGrocery
+//
+//  Created by ANI on 1/17/17.
+//  Copyright © 2017 sempie. All rights reserved.
+//
+
+import UIKit
+
+class GroceryItem: NSObject {
+    
+    var itemName :String!
+    var imgName  :String!
+    var qty      :Int! = 0
+    
+    var formattedQty :String {
+        return "Quantity: \(qty)"
+    }
+    
+    init(itemName: String, imgName: String, qty: Int) {
+        self.itemName = itemName
+        self.imgName = imgName
+        self.qty = qty
+    }
+
+}
--- iGrocery/ViewController.swift
+++ iGrocery/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let groceryArray = ["Milk", "Bread", "Eggs"]
+    var groceryArray = [GroceryItem]()
     
     //MARK: - Life Cycle Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        groceryArray = fillArray()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    //MARK: - Temp Methods
+    
+    func fillArray() -> [GroceryItem]{
+        let item1 = GroceryItem(itemName: "Milk", imgName: "Milk", qty: 1)
+        let item2 = GroceryItem(itemName: "Bread", imgName: "Bread", qty: 2)
+        let item3 = GroceryItem(itemName: "Eggs", imgName: "Eggs", qty: 12)
+        return [item1, item2, item3]
     }
 
 
@@ -33,7 +43,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let currentGroceryItem = groceryArray[indexPath.row]
-        cell.textLabel!.text = currentGroceryItem
+        cell.textLabel!.text = currentGroceryItem.itemName
         return cell
     }
 }
