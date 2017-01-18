fd584fe3e34a5b26152cb810f7b0c7560820a4b7
ANI <ani@gm009-4.local>
2017-01-17 19:17:08 -0500

    Basic tableview function complete.

--- iGrocery/ViewController.swift
+++ iGrocery/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let groceryArray = ["Milk", "Bread", "Eggs"]
+    
+    //MARK: - Life Cycle Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groceryArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let currentGroceryItem = groceryArray[indexPath.row]
+        cell.textLabel!.text = currentGroceryItem
+        return cell
+    }
 }
 
